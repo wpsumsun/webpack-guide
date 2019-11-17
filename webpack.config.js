fcfe241b99bee7dd7f045760bf354eedd5796e53
wangpeng <summer.wang@puman.com>
2019-11-17 21:01:10 +0800

    replace file-loader with url-loader

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,14 @@ module.exports = {
       },
       {
         test: /\.(png|jpg|jpeg|git)$/,
-        use: 'file-loader'
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10240
+            }
+          }
+        ]
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
