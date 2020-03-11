04a0beeb8f35178f01c8a54abfbdf284ce7ea41f
wangpeng <wangpeng06@kanzhun.com>
2020-03-11 21:54:07 +0800

    多页面配置(multiple page)

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,13 +5,41 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const glob = require("glob");
+
+const setMultiplePage = () => {
+  const entry = {};
+  const htmlWebpackPlugins = [];
+  const entryFiles = glob.sync(path.join(__dirname, "./src/*/index.js"));
+  entryFiles.map(entryFile => {
+    const match = entryFile.match(/src\/(.*)\/index\.js/);
+    const pageName = match && match[1];
+    entry[pageName] = entryFile;
+    htmlWebpackPlugins.push(
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, `src/${pageName}/index.html`),
+        filename: `${pageName}.html`,
+        chunks: [pageName], // Allows you to add only some chunks
+        inject: true, // true || 'head' || 'body' || false Inject all assets into the given template or templateContent. When passing true or 'body' all javascript resources will be placed at the bottom of the body element. 'head' will place the scripts in the head element
+        minify: {
+          html5: true, // Parse input according to HTML5 specifications
+          collapseWhitespace: true, // Collapse white space that contributes to text nodes in a document tree
+          preserveLineBreaks: false, // Always collapse to 1 line break (never remove it entirely) when whitespace between tags include a line break. Must be used in conjunction with collapseWhitespace=true
+          minifyCSS: true,
+          minifyJS: true,
+          removeComments: false
+        }
+      })
+    );
+  });
+  return { entry, htmlWebpackPlugins };
+};
+
+const { entry, htmlWebpackPlugins } = setMultiplePage();
 
 module.exports = {
   mode: 'production',
-  entry: {
-    index: './src/index.js',
-    search: './src/search.js'
-  },
+  entry,
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name]_[chunkhash:8].js'
@@ -88,40 +116,12 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: '[name]_[contenthash:8].css'
     }),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src/index.html'),
-      filename: 'index.html',
-      chunks: ['index'], // Allows you to add only some chunks
-      inject: true, // true || 'head' || 'body' || false Inject all assets into the given template or templateContent. When passing true or 'body' all javascript resources will be placed at the bottom of the body element. 'head' will place the scripts in the head element
-      minify: {
-        html5: true, // Parse input according to HTML5 specifications
-        collapseWhitespace: true, // Collapse white space that contributes to text nodes in a document tree
-        preserveLineBreaks: false, // Always collapse to 1 line break (never remove it entirely) when whitespace between tags include a line break. Must be used in conjunction with collapseWhitespace=true
-        minifyCSS: true,
-        minifyJS: true,
-        removeComments: false
-      }
-    }),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src/search.html'),
-      filename: 'search.html',
-      chunks: ['search'], // Allows you to add only some chunks
-      inject: true, // true || 'head' || 'body' || false Inject all assets into the given template or templateContent. When passing true or 'body' all javascript resources will be placed at the bottom of the body element. 'head' will place the scripts in the head element
-      minify: {
-        html5: true, // Parse input according to HTML5 specifications
-        collapseWhitespace: true, // Collapse white space that contributes to text nodes in a document tree
-        preserveLineBreaks: false, // Always collapse to 1 line break (never remove it entirely) when whitespace between tags include a line break. Must be used in conjunction with collapseWhitespace=true
-        minifyCSS: true,
-        minifyJS: true,
-        removeComments: false
-      }
-    }),
     new OptimizeCssAssetsPlugin({
       assetNameRegExp: /\.css$/g,
       cssProcessor: require('cssnano')
     }),
     new CleanWebpackPlugin()
-  ],
+  ].concat(htmlWebpackPlugins),
   optimization: {
     minimize: true, // production mode default true
     minimizer: [
@@ -131,4 +131,4 @@ module.exports = {
       })
     ],
   }
-}
+};
